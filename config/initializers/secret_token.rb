6922730d15490e800ae69fcea2ef655e4f1bf104
Patrick Robertson <patricksrobertson@gmail.com>
2013-03-25 10:38:01 -0400

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExOauthClient::Application.config.secret_token = '6a03c9fe3838a08fed0f68b92186f6c8e91099c71c7b23a8efec3e2bce2d8c5eb5f5c03a26f17d7d60b84172c8d4b61b471c4de44bf46cedbf263e4351de48ff'
