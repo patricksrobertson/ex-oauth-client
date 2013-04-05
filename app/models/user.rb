ce2ae61523bba757d321254bdc8c4445b34f8a51
Patrick Robertson <patricksrobertson@gmail.com>
2013-04-05 09:16:50 -0400

    Use the right crap.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
     create! do |user|
       user.provider = auth["provider"]
       user.uid = auth["uid"]
-      user.name = auth["user_info"]["email"]
+      user.name = auth["info"]["email"]
     end
   end
 end
