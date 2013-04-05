75ee52599dc1a5838a7430dab064f525ac16519d
Patrick Robertson <patricksrobertson@gmail.com>
2013-04-05 09:14:53 -0400

    Undo debug.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ActiveRecord::Base
   def self.create_with_omniauth(auth)
-    raise auth.to_yaml
     create! do |user|
       user.provider = auth["provider"]
       user.uid = auth["uid"]
-      user.name = auth["user_info"]["name"]
+      user.name = auth["user_info"]["email"]
     end
   end
 end
