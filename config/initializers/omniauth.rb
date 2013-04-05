a9234f12e02d662a3ce1322906c58f11216d5878
Patrick Robertson <patricksrobertson@gmail.com>
2013-04-05 07:58:47 -0400

    New provider!

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :icis, '2IXlJAF15JEoW28QWiPmfLHUjY2oMeLYpnn6K1le', 'i1pb8YnM06ogVVA8Uu67pLh4Ff7eDz2fw4WDRsu4'
+  provider :icis, '6d7c881e0fec6011b84bfe7244a9a120c5fe639e2dde6fb50e507316ba9d086f', '3e29944976db36b78cb236c6e1bcc9abbacf178f5c05f6d415423c4e1da71611'
 end
