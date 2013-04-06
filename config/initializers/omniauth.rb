fd9c4620694ac9d6c5157d02939245b10be1d303
Patrick Robertson <patricksrobertson@gmail.com>
2013-04-06 08:52:59 -0400

    Update the app info.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :icis, '6d7c881e0fec6011b84bfe7244a9a120c5fe639e2dde6fb50e507316ba9d086f', '3e29944976db36b78cb236c6e1bcc9abbacf178f5c05f6d415423c4e1da71611'
+  provider :icis, '747c5393782fc08fdf5a86ff11290b195bf9e4d82611f5972e3e1781ea36c042', '0400873f5fe889d37ebaf4a262c0572668052f4127f5ff54c2ccb37bc5b6efe2'
 end
